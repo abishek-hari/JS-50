836206be38433b6883ab1381ec8cb55c0c2b1ac7
Abishek <abishekhari09@gmail.com>
2023-06-29 05:39:54 +0530

    Dom

---
+++ JS_BASIC/DOM/script.js
@@ -0,0 +1,13 @@
+document.body.style.backgroundColor = "#aa55f8";
+
+const btn = document.getElementById("btns");
+btn.style.backgroundColor = "#45da5f";
+
+const result = document.querySelector(".result");
+
+const heading = document.createElement("h1");
+const text = document.createTextNode("HI, THERE EVERYONE");
+heading.appendChild(text);
+result.appendChild(heading);
+
+// Event Overview
